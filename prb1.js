e978a83ab731172a6929c82be5e5ffcb9dd0f514
TheRudraBro <rudramraj22@gmail.com>
2025-08-03 21:09:13 +0600

    first commit

---
+++ prb1.js
@@ -0,0 +1,3 @@
+var area = 800;
+var result = area/2
+console.log(result);
---
+++ prb2.js
@@ -0,0 +1,10 @@
+var money = 25000;
+if(money>=25000)
+{
+    console.log("Montu Can Buy a laptop")
+}else if(money>=10000)
+{
+    console.log("Montu can buy a cycle.")
+}else{
+    console.log("Montu can take a chocolate.")
+}
